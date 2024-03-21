5ce8d8a831742ba37cee7fd01ead6f2c0a48d2bc
BerindeanuDavid <david.berindeanu@emanuel.ro>
2024-03-21 12:35:22 +0200

    yellow face

--- script.js
+++ script.js
@@ -3,5 +3,25 @@ function setup(){
 }
 
 function draw(){
+    //head
     background(200);
+    stroke("black");
+    fill("yellow");
+    //eyes
+    circle(400,300,50);
+    stroke("black");
+    fill("pink");
+    ellipse(390,290,10,5);
+    ellipse(410,290,10,7);
+    stroke("cyan");
+    circle(390,290,2);
+    circle(410,290,2);
+    //mouth
+    stroke("black");
+    fill("grey")
+    arc(400,310,30,15,-3.14,Math.PI);
+    stroke("black");
+    fill("white")
+    arc(396,304,7,7,0,Math.PI);
+    arc(404,304,7,7,0,Math.PI);
 }    
