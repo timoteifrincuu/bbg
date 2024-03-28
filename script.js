fd2a9bbbe45e3e143f741bb14100a41b2edfbd1d
Timotei Frincu <timotei.frincu@emanuel.ro>
2024-03-28 11:50:12 +0200

    add parameters

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function setup() {
 function draw() {
     background(200);
     smileyFaceDavid();
-    smileyFaceTimo();
+    smileyFaceTimo(400, 300);
     smileyFaceCrys();
 }
 
@@ -61,28 +61,28 @@ function smileyFaceDavid() {
     arc(404, 304, 7, 7, 0, Math.PI);
 }
 
-function smileyFaceTimo() {
+function smileyFaceTimo(x,y) {
     //BODY
     stroke("black");
     fill("black");
-    circle(400, 300, 50);
+    circle(x, y, 50);
 
     //EYES
     fill("white");
     stroke("white");
     //eyes
-    ellipse(390, 290, 10, 5);
-    ellipse(410, 290, 10, 5);
+    ellipse(x-10, y-10, 10, 5);
+    ellipse(x+10, y-10, 10, 5);
     //pupils
     stroke("blue");
     fill("black");
-    circle(390, 290, 2);
-    circle(410, 290, 2);
+    circle(x-10, y-10, 2);
+    circle(x+10, y-10, 2);
 
     //MOUTH
     fill("white");
     stroke("white");
-    arc(400, 310, 40, 10, 0, Math.PI);
+    arc(x, y+10, 40, 10, 0, Math.PI);
 
     //NOSE
     fill("pink");
