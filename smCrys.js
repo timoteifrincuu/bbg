f2272cf5b71004923062bb832fb71d6f1f461d78
Crys <cristian.toanca@emanuel.ro>
2024-04-04 10:32:01 +0300

    smCrys file

---
+++ smCrys.js
@@ -0,0 +1,39 @@
+let smCrysX=400;
+let smCrysY=300;
+let directionCrysX=1;
+let directionCrysY=1;
+
+function checkLimitsSmCrys()
+{
+    if(smCrysX>=800||smCrysX<=0)
+    {   
+        directionCrysX*=-1;
+    }
+    if(smCrysY>=600||smCrysY<=0)
+    {
+        directionCrysY*=-1;
+    }
+}
+
+function smileyFaceCrys(x,y){
+    //head
+    stroke("black");
+    fill("orange");
+    circle(x, y, 100);
+    //eyes
+    stroke("cyan");
+    fill("cyan");
+    ellipse(x-15, y-18, 10, 5);
+    ellipse(x+5, y-18, 10, 7);
+    stroke("cyan");
+    circle(x-15, y-18, 2);
+    circle(x+5, y-18, 2);
+    //mouth
+    stroke("black");
+    fill("grey")
+    arc(x-5, y+2, 30, 15, -3.14, Math.PI);
+    stroke("blue");
+    fill("white")
+    arc(x-9, y-4, 7, 7, 0, Math.PI);
+    arc(x-1, y-4, 7, 7, 0, Math.PI);
+}
