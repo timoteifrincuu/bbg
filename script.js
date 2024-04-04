a263ec9a29cc57763e7fd0488aec975bfa051a64
Crys <cristian.toanca@emanuel.ro>
2024-04-04 10:36:05 +0300

    C file

--- script.js
+++ script.js
@@ -8,11 +8,6 @@ let smileyFaceTimoY = 300;
 let directionX = 10;
 let directionY = 15;
 
-let smCrysX=400;
-let smCrysY=300;
-let directionCrysX=70;
-let directionCrysY=10;
-
 function setup() {
     createCanvas(800, 600);
 }
@@ -28,45 +23,7 @@ function draw() {
     smileyFaceCrys(smCrysX=smCrysX+directionCrysX, smCrysY=smCrysY+directionCrysY);
 }
 
-function checkLimitsSmCrys()
-{
-    if(smCrysX>=800||smCrysX<=0)
-    {   
-        directionCrysX*=-1;
-    }
-    if(smCrysY>=600||smCrysY<=0)
-    {
-        directionCrysY*=-1;
-    }
-    checkLimitsSmTimo()
-    background(200);
-    smileyFaceDavid(smDavidX=smDavidX+directionDavidX,smDavidY=smDavidY+directionDavidY);
-    smileyFaceTimo(smileyFaceTimoX = smileyFaceTimoX + directionX, smileyFaceTimoY = smileyFaceTimoY + directionY);
-    smileyFaceCrys(400,300);
-}
 
-function smileyFaceCrys(x,y){
-    //head
-    stroke("black");
-    fill("orange");
-    circle(x, y, 100);
-    //eyes
-    stroke("black");
-    fill("yellow");
-    ellipse(x-15, y-18, 10, 5);
-    ellipse(x+5, y-18, 10, 7);
-    stroke("cyan");
-    circle(x-15, y-18, 2);
-    circle(x+5, y-18, 2);
-    //mouth
-    stroke("black");
-    fill("grey")
-    arc(x-5, y+2, 30, 15, -3.14, Math.PI);
-    stroke("blue");
-    fill("white")
-    arc(x-9, y-4, 7, 7, 0, Math.PI);
-    arc(x-1, y-4, 7, 7, 0, Math.PI);
-}
 
 function checkLimitsSmDavid()
 {
