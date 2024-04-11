4bb780db8cd139f1c28da35280a3a3a084096311
Crys <cristian.toanca@emanuel.ro>
2024-04-11 12:02:42 +0300

    nimic

--- smCrys.js
+++ smCrys.js
@@ -11,39 +11,55 @@ let smCrys= {
 
 function checkLimitsSmCrys()
 {
-    if(smCrys.x>=tableWidth - smCrys||smCrys.x <= 0+smCrys.ray)
-    {   
-        smCrys.directionX *= -1;
-    }
-    smCrys.x +=smCrys.directionX * smCrys.speedX;
-
-    if(smCrys.y>=tableWidth - smCrys.ray||smCrys.y<=0+ smCrys.ray)
+    if(smCrys.x >= tableWidth - smCrys.ray || smCrys.x <= 0 + smCrys.ray)
+    {
+        smCrys.directionX*=-1;
+    }   
+    
+    if(smCrys.y <= 0 + smCrys.ray)
     {
         smCrys.directionY*=-1;
+    }
+
+
+    //verifies paddle
+    if(
+        smCrys.y >= smCrys.y - smCrys.ray &&
+        smCrys.y <= smCrysy.y + smCrys.width &&
+        smCrys.x >=  smCrys.x - smCrys.ray &&
+        smCrys.x <=  smCrys.x + smCrys.width + smCrys.ray
+    )
+    {
+        smCrys.directionY *= -1;
     }
     smCrys.y += smCrys.directionY * smCrys.speedY;
 }
 
 function smileyFaceCrys(x,y){
-     // BODY
-  fill("Orange");
-  stroke("black");
-  circle(x, y, smCrys.ray * 2);
+         //BODY
+    stroke("black");
+    fill("black");
+    circle(x, y, smCrys.ray*1);
 
-  // EYE PUPILS
-  stroke("black");
-  fill("black");
-  circle(x - (smCrys.ray * 2) / 5, y - (smCrys.ray * 2) / 5, (smCrys.ray * 2) / 25);
-  circle(x + (smCrys.ray * 2) / 5, y - (smCrys.ray * 2) / 5, (smCrys.ray * 2) / 25);
+    //EYES
+    fill("white");
+    stroke("white");
+    //eyes
+    ellipse(x-(smCrys.ray*2)/5, y-(smCrys.ray * 2)/5, (smCrys.ray * 2)/5, (smCrys.ray * 2)/5);
+    ellipse(x+(smCrys.ray*2)/5, y-(smCrys.ray * 2)/5, (smCrys.ray * 2)/5, (smCrys.ray * 2)/5);
+    //pupils
+    stroke("blue");
+    fill("black");
+    circle(x - (smCrys.ray * 2) / 5, y - (smCrys.ray * 2) / 5, (smCrys.ray * 2) / 25);
+    circle(x + (smCrys.ray * 2) / 5, y - (smCrys.ray * 2) / 5, (smCrys.ray * 2) / 25);
 
-  // MOUNTH
-  stroke("green");
-  fill("gray");
-  arc(x, y + (smCrys.ray * 2) / 5, (smCrys.ray * 2) / 3, (smCrys.ray * 2) / 2, 0, Math.PI);
+    //MOUTH
+    fill("white");
+    stroke("white");
+    arc(x, y + (smCrys.ray * 2) / 5, (smCrys.ray * 2) / 1.6, (smCrys.ray * 2) / 5, 0, Math.PI);
 
-  // CHEEKS
-  stroke("darkred");
-  fill("red");
-  circle(x + (smCrys.ray * 2) / 2.5, y + (smCrys.ray * 2) / 10, (smCrys.ray * 2) / 7.14);
-  circle(x - (smCrys.ray * 2) / 2.5, y + (smCrys.ray * 2) / 10, (smCrys.ray * 2) / 7.14);
+    //NOSE
+    fill("pink");
+    circle(x + (smCrys.ray * 2) / 2.5, y + (smCrys.ray * 2) / 10, (smCrys.ray * 2) / 7.14);
+    circle(x - (smCrys.ray * 2) / 2.5, y + (smCrys.ray * 2) / 10, (smCrys.ray * 2) / 7.14);
 }
